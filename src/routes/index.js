c798519202779d2a0e0a66596500e98d6743d789
Nhat Thuong <doannhatthuong1110@gmail.com>
2023-06-20 09:40:51 +0700

    feat:add login page

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,8 @@ import Home from '@/pages/Home';
 import Services from '@/pages/Services';
 import Doctors from '@/pages/Doctors';
 import Appointment from '@/pages/Appointments';
-import Login from '@/pages/Account/Login';
-import RegisterPatientPage from '@/pages/Account/Register/Register';
+import Login from '@/pages/Login';
+import Register from '@/pages/Register';
 
 // Public routes
 const publicRoutes = [
@@ -16,7 +16,7 @@ const publicRoutes = [
     { path: '/services', component: Services, layout: OnlyHeader },
     { path: '/appointments', component: Appointment, layout: OnlyHeader },
     { path: '/login', component: Login, layout: null },
-    { path: '/register', component: RegisterPatientPage, layout: null },
+    { path: '/register', component: Register, layout: null },
 ];
 
 const privateRoutes = [];
