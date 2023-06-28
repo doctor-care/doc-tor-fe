307a12b9a2dd052f0c157aa4c1ce769b46fae8aa
Le Ngoc Tuan <cumeo154@gmail.com>
2023-06-28 21:03:02 +0700

    tuanln6-customer-edit

--- src/pages/patient/AsyncDataEditPatient.jsx
+++ src/pages/patient/AsyncDataEditPatient.jsx
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from 'react'
 import jwtDecode from 'jwt-decode'
 import EditPatient from './EditPatient';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function AsyncDataEditPatient() {
     const data = jwtDecode(localStorage.getItem("accessToken"));
     const [patient, setPatient] = useState({});
+    const navigate = useNavigate();
     useEffect(() => {
+        if (data?.sub) {
+            navigate("/");
+            return;
+        };
         axios.get(`http://localhost:8080/patient/username/${data.sub}`)
             .then(resp => {
                 setPatient(resp.data);
